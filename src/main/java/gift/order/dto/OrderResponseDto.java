cfb8274b6ab4922ad9be4e6b2c2641b3b66efe10
joongsunYoon <119833131+joongsunYoon@users.noreply.github.com>
2025-07-28 19:07:02 +0900

    refactor: record -> class로 수정 (fromEntity를 구현 하기 위해)

--- src/main/java/gift/order/dto/OrderResponseDto.java
+++ src/main/java/gift/order/dto/OrderResponseDto.java
@@ -1,11 +1,33 @@
 package gift.order.dto;
 
+import gift.order.entity.Order;
+
 import java.time.LocalDateTime;
 
-public record OrderResponseDto(
-        Long id,
-        Long optionId,
-        Long quantity,
-        String message,
-        LocalDateTime orderDateTime
-) {}
+public class OrderResponseDto{
+
+    private final Long id;
+    private final Long optionId;
+    private final Long quantity;
+    private final String message;
+    private final LocalDateTime orderDateTime;
+
+    private OrderResponseDto(Long id, Long optionId, Long quantity, String message, LocalDateTime orderDateTime) {
+        this.id = id;
+        this.optionId = optionId;
+        this.quantity = quantity;
+        this.message = message;
+        this.orderDateTime = orderDateTime;
+    };
+
+    public static OrderResponseDto fromEntity(Order order) {
+        return new OrderResponseDto(
+                order.getId(),
+                order.getOption().getId(),
+                order.getQuantity(),
+                order.getMessage(),
+                order.getOrderDateTime()
+        );
+    }
+
+}
