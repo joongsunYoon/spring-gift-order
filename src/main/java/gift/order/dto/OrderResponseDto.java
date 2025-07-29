f658986623e6b2bb6ec2c94afa766692cf3c7217
joongsunYoon <119833131+joongsunYoon@users.noreply.github.com>
2025-07-29 17:30:16 +0900

    refactor: 사용하기 편한 record로 변경

--- src/main/java/gift/order/dto/OrderResponseDto.java
+++ src/main/java/gift/order/dto/OrderResponseDto.java
@@ -4,21 +4,13 @@ import gift.order.entity.Order;
 
 import java.time.LocalDateTime;
 
-public class OrderResponseDto{
-
-    private final Long id;
-    private final Long optionId;
-    private final Long quantity;
-    private final String message;
-    private final LocalDateTime orderDateTime;
-
-    private OrderResponseDto(Long id, Long optionId, Long quantity, String message, LocalDateTime orderDateTime) {
-        this.id = id;
-        this.optionId = optionId;
-        this.quantity = quantity;
-        this.message = message;
-        this.orderDateTime = orderDateTime;
-    };
+public record OrderResponseDto(
+        Long id,
+        Long optionId,
+        Long quantity,
+        String message,
+        LocalDateTime orderDateTime
+){
 
     public static OrderResponseDto fromEntity(Order order) {
         return new OrderResponseDto(
