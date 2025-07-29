4e8870625cc9101f668e5b885d81878b1ba3b659
joongsunYoon <119833131+joongsunYoon@users.noreply.github.com>
2025-07-29 17:29:32 +0900

    feat: 주문 생성을 제외한 다른 기능 구현

--- src/main/java/gift/order/service/OrderService.java
+++ src/main/java/gift/order/service/OrderService.java
@@ -19,10 +19,13 @@ public class OrderService {
 
     public List<OrderResponseDto> findAll(Long memberId) {
 
+        return orderRepository.findByMemberId(memberId).stream().map(OrderResponseDto::fromEntity).toList();
     }
 
     public OrderResponseDto findOrder(Long id) {
-
+        return fromEntity(orderRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("아이디 값이 부적절 합니다")
+        ));
     }
 
     public OrderResponseDto createOrder(OrderRequestDto orderRequestDto) {
@@ -30,11 +33,28 @@ public class OrderService {
     }
 
     public OrderResponseDto updateOrder(Long id , OrderRequestDto orderRequestDto) {
+        Order order = orderRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("아이디 값이 부적절 합니다")
+        );
 
+        Option option = optionRepository.findById(orderRequestDto.optionId()).orElseThrow(
+                () -> new IllegalArgumentException("Option id 값이 부적절합니다")
+        );
+
+        Order savedOrder = new Order(
+                orderRequestDto.quantity(),
+                orderRequestDto.message(),
+                option,
+                order.getOrderDateTime(),
+                order.getMemberId()
+
+        );
+
+        return fromEntity(orderRepository.save(savedOrder));
     }
 
     public void deleteOrder(Long id) {
-
+        orderRepository.deleteById(id);
     }
 
 }
