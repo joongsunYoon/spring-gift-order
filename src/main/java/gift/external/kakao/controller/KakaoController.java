0477749a56c4c1770a356a98326333ce6298a199
joongsunYoon <119833131+joongsunYoon@users.noreply.github.com>
2025-07-31 17:49:16 +0900

    feat: 카카오 로그인 리다이렉트 기능 구현

--- src/main/java/gift/external/kakao/controller/KakaoController.java
+++ src/main/java/gift/external/kakao/controller/KakaoController.java
@@ -5,8 +5,15 @@ import gift.exception.GlobalExceptionHandler.ApiResponse;
 import gift.external.kakao.dto.KakaoTokenResponseDto;
 import gift.external.kakao.service.KakaoTokenService;
 import gift.member.entity.Member;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/oauth/kakao")
@@ -18,11 +25,27 @@ public class KakaoController {
         this.kakaoTokenService = kakaoTokenService;
     }
 
+    @Value("${kakao.client-id}")
+    private String clientId;
+
+    @Value("${kakao.redirect-uri}")
+    private String redirectUri;
+
+    @GetMapping("/authorize")
+    public void authorize(HttpServletResponse response) throws IOException {
+        String redirectUrl = "https://kauth.kakao.com/oauth/authorize"
+                + "?client_id=" + clientId
+                + "&redirect_uri=" + redirectUri + "/oauth/kakao/callback"
+                + "&response_type=code";
+
+        response.sendRedirect(redirectUrl);
+    }
+
     @GetMapping("/callback")
     public ResponseEntity<ApiResponse<KakaoTokenResponseDto>> callback(@RequestParam("code") String code,
                                                                        @LoginMember Member member) {
-        KakaoTokenResponseDto token = kakaoTokenService.getAccessToken(code,member.getId());
-        return ResponseEntity.ok(new ApiResponse<>(200,"토큰 발급 성공" , token));
+        KakaoTokenResponseDto token = kakaoTokenService.getAccessToken(code, member.getId());
+        return ResponseEntity.ok(new ApiResponse<>(200, "토큰 발급 성공", token));
     }
 
 }
